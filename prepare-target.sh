25b8f33801e3faa4556f36a6038daae88d40ee29
BurningC4 <wangweichen1995@hotmail.com>
2021-08-11 07:30:30 +0800

    Update prepare-target.sh

--- prepare-target.sh
+++ prepare-target.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-pacman -Syu --ask=4 arch-install-scripts
+pacman -Sy --ask=4 arch-install-scripts
 pacstrap -cGM /mnt base linux-raspberrypi4 crda dhcpcd dialog haveged nano net-tools netctl openssh raspberrypi-bootloader raspberrypi-bootloader-x vi which wireless_tools wpa_supplicant
 exit
