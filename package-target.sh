a806948d2f96fbb82ee3ab16201355f0d5dc3f7b
BurningC4 <wangweichen1995@hotmail.com>
2021-08-10 23:04:57 +0800

    Update package-target.sh

--- package-target.sh
+++ package-target.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 rm -rf mnt/etc/*- mnt/root/.bash_history mnt/root/.gnupg mnt/var/log/* mnt/var/lib/systemd/* 
-tar -czf ./release/ArchLinuxARM-rpi-4-aarch64-latest.tar.gz -C mnt/ .
+tar -czf ./ArchLinuxARM-rpi-4-aarch64-latest.tar.gz -C mnt/ .
 exit
