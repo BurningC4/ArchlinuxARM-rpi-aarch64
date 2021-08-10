fa9464754be829b8757d8c0a235784299fbbae05
BurningC4 <wangweichen1995@hotmail.com>
2021-08-10 21:56:24 +0800

    Create prepare-target.sh

---
+++ prepare-target.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+pacman-key --init
+pacman-key --populate archlinuxarm
+sed -i -e "s/^CheckSpace/#CheckSpace/g" /etc/pacman.conf
+pacman -Sy --ask=4 arch-install-scripts
+mkdir -p /mnt/root/boot
+pacstrap -cGM /mnt/root base linux-raspberrypi4 crda dhcpcd dialog haveged nano net-tools netctl openssh raspberrypi-bootloader raspberrypi-bootloader-x vi which wireless_tools wpa_supplicant
+exit
